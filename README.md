779dc36fc8254d59ae845cea4541acf22c0f4ed6
Rahul Anand <rahul.anand1222004@gmail.com>
2025-03-23 14:04:15 +0000

    added build

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ON-PON
-An online 1v1 pong game I am making to learn C++ and Networking.
+An online 1v1 pong game I am making to learn C++ and Networkingjj.
+
+Make sure you have following dependencies installed
+- cmake
+- sfml
+
+#### Clone the repo
+`git clone https://github.com/th3m45t3rm1nd/onpon.git`
+
+```
+cd onpon
+
+```
+
---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics/RenderWindow.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/Window/Event.hpp>
+#include <SFML/Window/VideoMode.hpp>
+#include <SFML/Window/Window.hpp>
+
+int main() {
+  sf::RenderWindow window(sf::VideoMode({800, 600}), "OnPon");
+
+  while (window.isOpen()) {
+    sf::Event event;
+
+    while (window.pollEvent(event)) {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    window.clear(sf::Color::Black);
+
+    window.display();
+  }
+}
